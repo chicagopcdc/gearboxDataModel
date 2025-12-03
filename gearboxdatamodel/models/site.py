b02519cafcb1d9dae1c53c49f4d2c1ecd718b382
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2025-12-03 12:39:56 -0600

    Added UniqueConstraint with coalesce to Site model.

--- gearboxdatamodel/models/site.py
+++ gearboxdatamodel/models/site.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, DateTime, UniqueConstraint, ForeignKey, DECIMAL, Index
+from sqlalchemy import Column,Integer, String, DateTime, UniqueConstraint, ForeignKey, DECIMAL, Index, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, mapped_column
 from sqlalchemy.sql import func
 
@@ -23,14 +23,28 @@ class Site(Base):
     # The following solution will work on postgres 15 or later
     # UniqueConstraint(name, location_lat, location_long, name="site_uix", postgresql_nulls_not_distinct=True)
 
+    #__table_args__ = (
+    #    Index(
+    #        'idx_name_lat_long',
+    #        name,
+    #        func.coalesce(location_lat, 0),
+    #        func.coalesce(location_long, 0),
+    #        unique=True
+    #    ),
+    #)
+
     __table_args__ = (
-        Index(
-            'idx_name_lat_long',
+        UniqueConstraint(
             name,
-            func.coalesce(location_lat, 0),
-            func.coalesce(location_long, 0),
-            unique=True
-        ),
+            location_lat,
+            location_long,
+            name='idx_name_lat_long',
+            postgresql_where=(
+                Column('name'),
+                Column('location_lat').coalesce(0),
+                Column('location_long').coalesce(0)
+            )
+        )
     )
 
     studies = relationship("SiteHasStudy", back_populates="site")
