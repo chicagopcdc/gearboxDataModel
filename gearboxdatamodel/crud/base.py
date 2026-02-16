4aad1a31627282a432b0d3cec8ab96f0aef8fc58
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2026-02-16 12:11:06 -0600

    Error msg for testing in base.py

--- gearboxdatamodel/crud/base.py
+++ gearboxdatamodel/crud/base.py
@@ -118,12 +118,15 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             await db.commit()
             return db_obj
         except IntegrityError as e:
+            print(f"CREATE CRUD IntegrityError ERROR {e}")
             logger.error(f"CREATE CRUD IntegrityError ERROR {e}")
             raise HTTPException(status.HTTP_409_CONFLICT, f"INTEGRITY SQL ERROR: {type(e)}: {e}")
         except exc.SQLAlchemyError as e:
+            print(f"CREATE CRUD SQLAlchemyError ERROR {e}")
             logger.error(f"CREATE CRUD SQLAlchemyError ERROR {e}")
             raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, f"SQL ERROR: {type(e)}: {e}")        
         except Exception as e:
+            print(f"CREATE CRUD OTHER ERROR {e}")
             logger.error(f"CREATE CRUD OTHER ERROR {e}")
             raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, f"OTHER ERROR: {type(e)}: {e}")        
 
