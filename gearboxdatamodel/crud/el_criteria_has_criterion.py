d650b3036aa03061908231e041a69b47cdc85f55
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2026-03-24 15:45:28 -0500

    Added missing await in el_criteria_has_criterion crud method.

--- gearboxdatamodel/crud/el_criteria_has_criterion.py
+++ gearboxdatamodel/crud/el_criteria_has_criterion.py
@@ -18,9 +18,10 @@ class CRUDElCriteriaHasCriterion(
 ):
 
     async def get_echc_by_ec_id(self, current_session: Session, ec_id: int) -> List[ElCriteriaHasCriterion]:
-        return await self.get_multi(
+        retval = await self.get_multi(
             current_session,
             where=f"ElCriteriaHasCriterion.eligibility_criteria_id = {ec_id}",
         )
+	return retval
 
 el_criteria_has_criterion_crud = CRUDElCriteriaHasCriterion(ElCriteriaHasCriterion)
